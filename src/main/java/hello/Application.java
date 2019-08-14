62caf8970892923afae41e42c55f59fa67ade4b3
Rory Fielding <rory-f@hotmail.co.uk>
2019-08-14 13:51:47 +0100

    test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,12 +2,16 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @RestController
+@PropertySources({ @PropertySource(value = "classpath:application.properties") })
 public class Application {
 
     @RequestMapping("/")
